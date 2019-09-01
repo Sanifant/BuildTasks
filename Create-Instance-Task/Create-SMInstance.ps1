6996ee125218f6ed10c9f01b5b0c7673726d6704
Stefan Schulz <stefan@openelp.de>
2019-09-01 18:28:58 +0200

    Updated Version handling on Instance Creation.

--- Create-Instance-Task/Create-SMInstance.ps1
+++ Create-Instance-Task/Create-SMInstance.ps1
@@ -22,40 +22,70 @@ try{
     [String]$LicenseServer    = Get-VstsInput -Name LicenseServer
     [boolean]$TrustedConnection = Get-VstsInput -Name TrustedConnection -AsBool
 
-    $VersionRootFolder = (Get-ItemProperty -Path (Join-Path $VersionRootKey $SMVersion)).RootFolder
-    $VersionExeFolder = Join-Path $VersionRootFolder "EXE"
-
-    Write-VstsTaskDebug "Instancename: $InstanceName";
-    Write-VstsTaskDebug "SampleManager Version: $SMVersion";
-    Write-VstsTaskDebug "Root Folder for Version $SMVersion is $VersionRootFolder"
-    Write-VstsTaskDebug "Exe Folder for Version $SMVersion is $VersionExeFolder"
-    Write-VstsTaskDebug "Database Server: $DataBaseServer";
-    Write-VstsTaskDebug "Database: $DataBase";
-    if ($TrustedConnection) {
-        $AdoConnectionString = "Provider=SQLNCLI11;Server=$DataBaseServer;Database=$DataBase;Trusted_Connection=yes;MARS Connection=true;"
+    if($null -eq $InstanceName){
+        Write-VstsTaskError "Instance not specified"
     }
+    if($null -eq $SMVersion){
+        Write-VstsTaskError "SampleManager Version not specified"
+    }
+    if($null -eq $DataBaseServer){
+        Write-VstsTaskError "DataBaseServer not specified"
+    }
+    if($null -eq $DataBase){
+        Write-VstsTaskError "DataBase not specified"
+    }
+
+    if($SMVersion -match '(?<major>12).(?<minor>[1-9]).(?<patch>[0-9])?'){
+        $Major = $Matches.major
+        $Minor = $Matches.minor
+        $Patch = $Matches.patch
+        if("0" -eq $Patch){
+            $SMVersion = "$Major.$Minor"
+        }
+
+        if(Test-Path -Path $VersionRootKey){
+
+            $VersionRootFolder = (Get-ItemProperty -Path (Join-Path $VersionRootKey $SMVersion)).RootFolder
+            $VersionExeFolder = Join-Path $VersionRootFolder "EXE"
+
+            Write-VstsTaskDebug "Instancename: $InstanceName";
+            Write-VstsTaskDebug "SampleManager Version: $SMVersion";
+            Write-VstsTaskDebug "Root Folder for Version $SMVersion is $VersionRootFolder"
+            Write-VstsTaskDebug "Exe Folder for Version $SMVersion is $VersionExeFolder"
+            Write-VstsTaskDebug "Database Server: $DataBaseServer";
+            Write-VstsTaskDebug "Database: $DataBase";
+            if ($TrustedConnection) {
+                $AdoConnectionString = "Provider=SQLNCLI11;Server=$DataBaseServer;Database=$DataBase;Trusted_Connection=yes;MARS Connection=true;"
+            }
+            else {
+                $AdoConnectionString = "Provider=SQLNCLI11;Server=$DataBaseServer;Database=$DataBase;User Id=$DataBaseUser;Password=$DataBasePassword;MARS Connection=true"
+            }
+            Write-VstsTaskDebug "Using connectionstring: $AdoConnectionString";
+            Write-VstsTaskDebug "Using Licenseserver: $LicenseServer";
+
+            $builder = Join-Path $VersionExeFolder 'BuildInstance.exe';
+            $cmdArgList = @(
+                "-n", "$InstanceName",
+                "-dbs", "$DataBaseServer",
+                "-db", "$DataBase"
+                "-ls", "$LicenseServer"
+                "-ado", "$AdoConnectionString"
+            )
+
+            Write-VstsTaskDebug "Executing $builder";
+            $result = & $builder $cmdArgList;
+            Write-VstsTaskDebug "\t$result";
+
+            if($LASTEXITCODE -eq 1)
+            {
+                Write-VstsTaskError "Instance was not builded properly => $result"
+            }
+        }
+        else {
+            Write-VstsTaskError "SampleManager not installed"
+        }
     else {
-        $AdoConnectionString = "Provider=SQLNCLI11;Server=$DataBaseServer;Database=$DataBase;User Id=$DataBaseUser;Password=$DataBasePassword;MARS Connection=true"
-    }
-    Write-VstsTaskDebug "Using connectionstring: $AdoConnectionString";
-    Write-VstsTaskDebug "Using Licenseserver: $LicenseServer";
-
-    $builder = Join-Path $VersionExeFolder 'BuildInstance.exe';
-    $cmdArgList = @(
-        "-n", "$InstanceName",
-        "-dbs", "$DataBaseServer",
-        "-db", "$DataBase"
-        "-ls", "$LicenseServer"
-        "-ado", "$AdoConnectionString"
-    )
-
-    Write-VstsTaskDebug "Executing $builder";
-    $result = & $builder $cmdArgList;
-    Write-VstsTaskDebug "\t$result";
-
-    if($LASTEXITCODE -eq 1)
-    {
-        Write-VstsTaskError "Instance was not builded properly => $result"
+        Write-VstsTaskError "Version $SMVersion is not valid"
     }
 
 } finally {
--- Tests/Test-Package.ps1
+++ Tests/Test-Package.ps1
@@ -1,5 +1,15 @@
-Import-Module ..\BuildAndReleaseTask\ps_modules\VstsTaskSdk
-# Task variable 'Build.SourcesDirectory':
-$env:BUILD_SOURCESDIRECTORY = "D:\Temp"
+[CmdletBinding()]
+param()
 
-Invoke-VstsTaskScript -ScriptBlock ([scriptblock]::Create('.\BuildAndReleaseTask\Create-SMInstance.ps1')) -Verbose
+. $PSScriptRoot\lib\Initialize-Test.ps1
+
+Register-Mock Trace-VstsEnteringInvocation
+Register-Mock Trace-VstsLeavingInvocation
+Register-Mock Import-VstsLocStrings
+Register-Mock Get-VstsInput { 'VGSM' } -- -Name InstanceName
+Register-Mock Write-VstsTaskError
+
+$actual = & $PSScriptRoot\..\Create-Instance-Task\Create-SMInstance.ps1
+write-host $actual
+
+Assert-WasCalled Write-VstsTaskError
